2d3160f98d1816ce7b6adb323c07d8606f9d0a79
Ghosteteu <157996358+Ghosteteu@users.noreply.github.com>
2024-01-28 18:53:04 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+/*
+
+objetivo 1 - quando o usuario clicar no botão de seleção de plataforma deve abrir uma caixa com os botões de seleção de plataforma
+    
+
+*/
+
+    const botao = document.querySelector(".btn-plataforma");
+    const elementoPlataforma = document.querySelector(".btn-plataforma .plataformas")
+
+    botao.addEventListener("click", () => 
+    const botaoEstaAberto = elementoPlataforma.classList.contains("ativo");
+    else
+    elementoPlataforma.classList.toggle"(ativo");
+    }    
+});
